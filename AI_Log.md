cb18fbbc87096cd6f6bdea21868e894a8d2e6c63
Dusan Vicentic <vicenticdusan97@gmail.com>
2026-06-14 20:25:00 -0500

    part 2.1 found and printed basic image stats, updated AI log

--- AI_Log.md
+++ AI_Log.md
@@ -1,3 +1,3 @@
 | Date and Time | Prompt | Tool | Response Synopsis | Change |
 | ------------- | ------ | ---- | ----------------- | ------ |
-|
+| 06/14/26 5:55 | What is the best way to calculate descriptive statistics of an image in python? | Gemini | Use OpenCV to split image into arrays, use Numpy and Scipy for stats | Imported libraries, split image into BGR channels, and calculated desc stats |
--- main.py
+++ main.py
@@ -4,3 +4,24 @@ import scipy.stats as stats
 import matplotlib as plt
 import os
 
+img = cv2.imread('/Users/dusanvicentic/WSU/Image Analysis & Computer Vision/DusanVicentic-CS898BA-Project1/HW1_IMG_CS898BA.png')
+
+b, g, r = cv2.split(img)
+channels = {'Blue': b, 'Green': g, 'Red': r}
+
+for name, channel in channels.items():
+    flat = channel.flatten()
+
+print(f"--- Stats for {name} Channel ---")
+print(f'Min: {np.min(flat)}')
+print(f'Max: {np.max(flat)}')
+print(f'Range: {np.max(flat)}')
+print(f'Average: {np.mean(flat):.2f}')
+print(f'Median: {np.median(flat)}')
+
+mode_result = stats.mode(flat, keepdims=False)
+print(f'Mode: {mode_result.mode}')
+print(f'Skew: {stats.skew(flat):.2f}')
+
+print(f'Std Dev: {np.std(flat):.2f}')
+print(f'Variance: {np.var(flat):.2f}\n')
